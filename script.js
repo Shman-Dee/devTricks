fea1395fed37a829a3d2081d5cab77766066d86b
Andrew McInally <realfunnyestate@yahoo.com>
2021-11-26 23:20:29 -0700

    yup

---
+++ script.js
@@ -0,0 +1,35 @@
+const dogs = [
+  { name: "Snickers", age: 2 },
+  { name: "hugo", age: 8 },
+];
+
+function makeGreen() {
+  const p = document.querySelector("p");
+  p.style.color = "#BADA55";
+  p.style.fontSize = "50px";
+}
+//most of these don't seem too useful for my level of coding...
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing
